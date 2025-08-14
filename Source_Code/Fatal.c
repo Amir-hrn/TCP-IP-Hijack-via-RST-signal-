5ca71a5ed7dc7f944a900f0f50eeca1f34a1d870
Amirhossein <2012.haririan@gmail.com>
2025-08-14 20:45:34 +0330

    Fatal.c

---
+++ Source_Code/Fatal.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// A function to display an error message and then exit
+void fatal(char *message) {
+   char error_message[100];
+
+   strcpy(error_message, "[!!] Fatal Error ");
+   strncat(error_message, message, 83);
+   perror(error_message);
+   exit(-1);
+}
